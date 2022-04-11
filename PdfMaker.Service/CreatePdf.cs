bd3083569015b241cda854797fb9547536aac106
Bashir Momen <bashir.momen@yahoo.com>
2022-04-11 19:58:19 +0200

    Added pdfSharpCore

---
+++ PdfMaker.Service/CreatePdf.cs
@@ -0,0 +1,21 @@
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+
+namespace PdfMaker.Service
+{
+    public class CreatePdf
+    {
+        public CreatePdf()
+        {
+            var document = new PdfDocument();
+
+            var page = document.AddPage();
+
+            var gfx = XGraphics.FromPdfPage(page);
+
+            var font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+        }
+    }
+}
