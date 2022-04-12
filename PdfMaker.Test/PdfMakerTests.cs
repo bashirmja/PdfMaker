41029c80d8c7f288b78fbc66ac452efa819e3454
Bashir Momen <bashir.momen@yahoo.com>
2022-04-12 16:39:53 +0200

    Updat tests

--- PdfMaker.Test/PdfMakerTests.cs
+++ PdfMaker.Test/PdfMakerTests.cs
@@ -1,9 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,8 +10,6 @@ namespace PdfMaker.Test
 {
     public class PdfMakerTests
     {
-        private readonly Uri BaseAddress = new Uri("https://localhost:7131");
-
         [Fact]
         public async Task CreatePDFAsync()
         {
@@ -36,43 +33,21 @@ namespace PdfMaker.Test
 
 
             var client = new HttpClient();
-            client.BaseAddress = BaseAddress;
-            var httpResponse = await client.PostAsync("/createpdf", multipartFormContent);
+            var baseAddress = "https://localhost:7131";
+            client.BaseAddress = new Uri(baseAddress);
 
-            string resultContent = await httpResponse.Content.ReadAsStringAsync();
-            Assert.Equal("Hello", resultContent);
+            var postResponse = await client.PostAsync("/pdf", multipartFormContent);
+            string fileId = await postResponse.Content.ReadAsStringAsync();
+
+            Process.Start(new ProcessStartInfo(baseAddress + "/pdf/" + fileId) { UseShellExecute = true });
+
+            Assert.NotEmpty(fileId);
         }
 
-
-
         [Fact]
-        public async Task TestPostAsync()
+        public void TrueTest()
         {
-            var payload = new
-            {
-                Title = "COBOD"
-            };
-            var stringPayload = JsonSerializer.Serialize(payload);
-            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-
-            var client = new HttpClient();
-            client.BaseAddress = BaseAddress;
-            var httpResponse = await client.PostAsync("/testpost", httpContent);
-
-            string resultContent = await httpResponse.Content.ReadAsStringAsync();
-            Console.WriteLine(resultContent);
-            Assert.Equal("COBOD", resultContent);
-        }
-
-
-        [Fact]
-        public async Task TestGetAsync()
-        {
-            using var client = new HttpClient();
-            client.BaseAddress = BaseAddress;
-            var result = await client.GetAsync("/testget");
-            string resultContent = await result.Content.ReadAsStringAsync();
-            Assert.Equal("GetOK", resultContent);
+            Assert.True(true);
         }
     }
 }
